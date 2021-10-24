6ff32bdf0f6053a81de52687154246f41a046683
JihyeCha <83751278+JihyeCha@users.noreply.github.com>
2021-10-25 01:07:12 +0900

    Create 3-02.py

---
+++ 3-02.py
@@ -0,0 +1,7 @@
+## 제 3장 - 도전문제2
+# - 섭씨온도를 화씨온도로 변환하는 프로그램을 작성해보자.
+
+ctemp = int(input("섭씨온도: "))   # ctemp는 Celsius temperature을 줄인 말
+ftemp = 9/5 * ctemp + 32   # ftemp는 Fahrenheit temperature을 줄인 말
+print("화씨온도: ", ftemp)
+
