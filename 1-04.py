bacbb0996cbc8185eaa6bd158444b40cae5083f7
JihyeCha <83751278+JihyeCha@users.noreply.github.com>
2021-10-24 23:24:21 +0900

    Create 1-04.py

---
+++ 1-04.py
@@ -0,0 +1,19 @@
+## 제1장 - 도전문제 4
+# - 화살표를 움직여서 육각형을 그려보자. 회전하는 각도는 몇 도로 하여야 하는가?
+
+# 육각형의 경우엔 외각이 60도를 이뤄야 내각 120도가 만들어짐.
+
+import turtle
+t = turtle.Turtle()
+t.shape("turtle");
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
